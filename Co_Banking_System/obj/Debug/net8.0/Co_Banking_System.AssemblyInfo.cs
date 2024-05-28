b467beb9314df1fb3fea0454d407463dcb0a4fab
winniegithubrit <winnie.jomo@student.moringaschool.com>
2024-05-28 20:46:25 +0300

    completed

--- Co_Banking_System/obj/Debug/net8.0/Co_Banking_System.AssemblyInfo.cs
+++ Co_Banking_System/obj/Debug/net8.0/Co_Banking_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Co_Banking_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d183cff3fb34a43cc28166dff57d9396ceb208d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+557a76825c2125aa0d5ed95c6a126e8965c46f54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Co_Banking_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Co_Banking_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
